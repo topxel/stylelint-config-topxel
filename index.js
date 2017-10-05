e8b288d60f03d4376f84cf9ab2ada0d86211f0e5
Nerea Marta <nerea@topxel.com>
2017-10-05 09:37:37 +0200

    Modified style-order rules and updated package.json

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var merge = require('merge');
+const merge = require('merge');
 
 module.exports = {
   plugins: ['stylelint-order'],
   rules: merge(
-    require('./rules/possibleErrors')
-    require('./rules/limitLanguageFeatures')
-    require('./rules/style')
+    require('./rules/possibleErrors'),
+    require('./rules/limitLanguageFeatures'),
+    require('./rules/style'),
     require('./rules/plugins/order')
   )
-},
+};
